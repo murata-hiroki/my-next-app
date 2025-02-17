e077e1f98c196b10bc036424567fa64bfa4e39ae
murata-hiroki <hirokinaoki2000@gmail.com>
2025-02-18 03:37:14 +0900

    ニュースセクション作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,14 @@ import ChairmanMessage from "./components/ChairmanMessage";
 import Members from "./components/Members";
 import Services from "./components/Services";
 import Blog from "./components/Blog";
+import News from "./components/News";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <main>
         <Hero />
+        <News />
         <Sponsors />
         <About />
         <ChairmanMessage />
